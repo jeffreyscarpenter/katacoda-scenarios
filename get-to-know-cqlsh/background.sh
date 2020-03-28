8ce271c9fbe668807c89c9f4501be021e76b5038
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2020-03-28 16:03:23 -0700

    set environment

--- get-to-know-cqlsh/background.sh
+++ get-to-know-cqlsh/background.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#CASSANDRA_VERSION="3.11.6"
-
-#echo "export CASSANDRA_VERSION=${CASSANDRA_VERSION}" >>~/.bashrc
-
 wget https://downloads.apache.org/cassandra/3.11.6/apache-cassandra-3.11.6-bin.tar.gz
 tar xzf apache-cassandra-3.11.6-bin.tar.gz
 apache-cassandra-3.11.6/bin/cassandra -R
