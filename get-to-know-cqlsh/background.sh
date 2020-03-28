a6f39213aece03969ff2913ce735db5bac4c92e8
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2020-03-28 14:20:17 -0700

    paths

--- get-to-know-cqlsh/background.sh
+++ get-to-know-cqlsh/background.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "export JAVA_HOME=java-8-openjdk-amd64" >>~/.bashrc
-echo "export PATH=$JAVA_HOME/bin:$PATH" >>~/.bashrc
+#echo "export JAVA_HOME=java-8-openjdk-amd64" >>~/.bashrc
+#echo "export PATH=$JAVA_HOME/bin:$PATH" >>~/.bashrc
 wget https://downloads.apache.org/cassandra/3.11.6/apache-cassandra-3.11.6-bin.tar.gz
 tar xzf apache-cassandra-3.11.6-bin.tar.gz
 apache-cassandra-3.11.6/bin/cassandra -R
