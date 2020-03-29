d16f7001f83182825481332c301d5a9b740b821f
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2020-03-28 19:25:59 -0700

    bad quote

--- get-to-know-cqlsh/background.sh
+++ get-to-know-cqlsh/background.sh
@@ -2,7 +2,7 @@
 
 export PATH=$JAVA_HOME:$PATH
 
-wget http://archive.apache.org/dist/cassandra/3.11.6/apache-cassandra-3.11.6-bin.tar.gz"
+wget http://archive.apache.org/dist/cassandra/3.11.6/apache-cassandra-3.11.6-bin.tar.gz
 tar xzf apache-cassandra-3.11.6-bin.tar.gz
 apache-cassandra-3.11.6/bin/cassandra -R
 while [ `grep "Starting listening for CQL clients" apache-cassandra-3.11.6/logs/system.log | wc -l` -lt 1 ]; do
