cc5b5d08a30647638c55b67214892908d75c1396
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2020-04-24 11:18:16 -0700

    4.0alpha4

--- get-to-know-cqlsh/background.sh
+++ get-to-know-cqlsh/background.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
-wget http://www.apache.org/dyn/closer.lua/cassandra/4.0-alpha4/apache-cassandra-4.0-alpha4-bin.tar.gz
+wget https://apache.cs.utah.edu/cassandra/4.0-alpha4/apache-cassandra-4.0-alpha4-bin.tar.gz
 tar xzf apache-cassandra-4.0-alpha4-bin.tar.gz
 apache-cassandra-4.0-alpha4/bin/cassandra -R
 while [ `grep "Starting listening for CQL clients" apache-cassandra-4.0-alpha4/logs/system.log | wc -l` -lt 1 ]; do
