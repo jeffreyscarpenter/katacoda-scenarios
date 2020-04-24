f3473e513e7d5deabcd0b4b4505f34e8337d213b
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2020-04-24 10:58:48 -0700

    4.0alpha4

--- get-to-know-cqlsh/background.sh
+++ get-to-know-cqlsh/background.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-
-#export PATH=$JAVA_HOME:$JAVA_HOME/bin:$PATH
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
-touch ~/.mavenrc
-export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre" >>~/.mavenrc
-
-wget http://archive.apache.org/dist/cassandra/3.11.6/apache-cassandra-3.11.6-bin.tar.gz
-tar xzf apache-cassandra-3.11.6-bin.tar.gz
-apache-cassandra-3.11.6/bin/cassandra -R
-while [ `grep "Starting listening for CQL clients" apache-cassandra-3.11.6/logs/system.log | wc -l` -lt 1 ]; do
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+wget http://www.apache.org/dyn/closer.lua/cassandra/4.0-alpha4/apache-cassandra-4.0-alpha4-bin.tar.gz
+tar xzf apache-cassandra-4.0-alpha4-bin.tar.gz
+apache-cassandra-4.0-alpha4/bin/cassandra -R
+while [ `grep "Starting listening for CQL clients" apache-cassandra-4.0-alpha4/logs/system.log | wc -l` -lt 1 ]; do
    sleep 15
 done
-
 echo "done" >> /opt/katacoda-background-finished
--- get-to-know-cqlsh/intro.md
+++ get-to-know-cqlsh/intro.md
@@ -4,7 +4,7 @@ In this scenario, we'll get to know Cassandra's command line shell `cqlsh`.
 
 We've already installed Cassandra on this instance for you and started a running instance, now all that remains is to start the shell, and you'll be ready to learn some basic commands:
 
-`./apache-cassandra-4.0-alpha3/bin/cqlsh`{{execute}}
+`./apache-cassandra-4.0-alpha4/bin/cqlsh`{{execute}}
 
 ![Reservation domain](assets/reservation-domain.png)
 
